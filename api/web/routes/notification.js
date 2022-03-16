d64e5288e4b6a588f743df54b32f338d8104b766
sunilkumar019 <sunilmehra.webhopers@gmail.com>
2022-03-16 15:26:03 +0530

    adding

--- api/web/routes/notification.js
+++ api/web/routes/notification.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const notification = require('../handlers/notification.js')
+const notification = require('../handlers/notification')
 const check_auth = require("../../../core/middleware/check-auth-admin");
 
 router.post('/', check_auth, notification)
